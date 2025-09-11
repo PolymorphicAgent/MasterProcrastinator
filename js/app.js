099930bfcf8cf415d81fadcd0215950723381b9c
PolymorphicAgent <polymorphicagent07@gmail.com>
2025-09-11 08:48:23 -0400

    attempt particle momentum fix #5

--- js/app.js
+++ js/app.js
@@ -713,7 +713,7 @@ window.addEventListener('keydown', (e) => {
 const canvas = document.getElementById('three-canvas');
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth/window.innerHeight, 0.1, 1000);
-const renderer = new THREE.WebGLRenderer({ canvas, alpha: true });
+let renderer = new THREE.WebGLRenderer({ canvas, alpha: true });
 renderer.setSize(window.innerWidth, window.innerHeight);
 renderer.setPixelRatio(window.devicePixelRatio);
 document.body.appendChild(renderer.domElement);
