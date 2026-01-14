b2e3df7602278ae305ab92359cb9a9919fedc9bb
PolymorphicAgent <polymorphicagent07@gmail.com>
2026-01-14 11:16:50 -0500

    i'm a big dummy

--- js/utils.js
+++ js/utils.js
@@ -158,6 +158,20 @@ function ensurePathLength(inpString, length){
     else return inpString;
 }
 
+function simpleMarkdown(md) {
+  return md
+    .replace(/^### (.*$)/gim, '<h3>$1</h3>')
+    .replace(/^## (.*$)/gim, '<h2>$1</h2>')
+    .replace(/^# (.*$)/gim, '<h1>$1</h1>')
+    .replace(/^\> (.*$)/gim, '<blockquote>$1</blockquote>')
+    .replace(/\*\*(.*?)\*\*/gim, '<b>$1</b>')
+    .replace(/\*(.*?)\*/gim, '<i>$1</i>')
+    .replace(/`([^`]+)`/gim, '<code>$1</code>')
+    .replace(/^\s*[-*] (.*$)/gim, '<ul><li>$1</li></ul>')
+    .replace(/\n$/gim, '<br>')
+    .replace(/\[(.*?)\]\((.*?)\)/gim, '<a href="$2" target="_blank">$1</a>');
+}
+
 async function showReadme() {
   const modal = document.getElementById('readmeModal');
   const content = document.getElementById('readmeContent');
